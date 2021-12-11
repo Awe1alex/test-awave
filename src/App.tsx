c21302f78365591c50af875e88c0ab4705b3a249
Awe1alex <aleksejs.birjulins@gmail.com>
2021-12-11 14:34:57 +0200

    router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.scss'
+import Layout from './components/Layout'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import Users from './pages/Users'
 
 function App() {
-  return <div>App</div>
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/logout' element={<Logout />} />
+      </Route>
+    </Routes>
+  )
 }
 
 export default App
